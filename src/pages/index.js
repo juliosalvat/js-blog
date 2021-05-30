cfb078e8968b8a851364452791eb66f3c29e242e
juliosalvat <code@juliosalvat.com>
2021-05-29 17:34:18 -0700

    Finished Gatsby Tutorial Part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout>
+      <p>Hey, I'm Julio Salvat and welcome to my blog.</p>
+      <StaticImage
+        alt="Julio Salvat"
+        src="https://juliosalvat.com/images/profile.png"
+      />
     </Layout>
   )
 }
